131b61ac1c08a3304479dec5ff90b7cfe21f8bca
GeneralFinny <143330234+GeneralFinny@users.noreply.github.com>
2023-10-10 17:14:57 -0700

    Added all necessary files to run Celita_Server.py
    requirments.txt also included

---
+++ Front_End/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { spawn } = require("child_process");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "Front_End/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Run your Python Flask app
+const pythonProcess = spawn("python", ["app.py"]);
+pythonProcess.stdout.on("data", (data) => {
+  console.log(data.toString());
+});
